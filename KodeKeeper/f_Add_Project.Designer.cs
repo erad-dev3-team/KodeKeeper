3f134d8ef4abb0e76470efb490c0a981bd7cc435
Radics Márton <marton.radics@dpdgroup.com>
2019-02-05 14:24:02 +0100

    edf

--- KodeKeeper/f_Add_Project.Designer.cs
+++ KodeKeeper/f_Add_Project.Designer.cs
@@ -258,7 +258,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(338, 228);
+			this.ClientSize = new System.Drawing.Size(337, 228);
 			this.Controls.Add(this.btn_DeleteProject);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.btn_Cancel);
